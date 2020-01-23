e9e6c36cebb9c14371605ab2bc3c31ef447a4cf5
CristianPoma <pomacristian421@gmail.com>
2020-01-23 14:29:57 -0500

    23/01/2020

--- Model/Usuario.js
+++ Model/Usuario.js
@@ -1,28 +1,50 @@
-const sql= require('../Base/Db')
-const usuario=function(Usuario){
-    this.id_usuario=Usuario.id_usuario;
-    this.cedula=Usuario.cedula;
-    this.nombre=Usuario.nombre;
-    this.apellido-Usuario.apellido;
-    this.direccion=Usuario.direccion;
-    this.telefono=Usuario.telefono;
-    this.correo=Usuario.correo;
-    this.tipo=Usuario.tipo;
-}; 
+const sql = require('../Base/Db')
+const usuario = function (Usuario) {
+    this.id_usuario = Usuario.id_usuario;
+    this.cedula = Usuario.cedula;
+    this.nombre = Usuario.nombre;
+    this.apellido = Usuario.apellido;
+    this.direccion = Usuario.direccion;
+    this.telefono = Usuario.telefono;
+    this.correo = Usuario.correo;
+    this.tipo = Usuario.tipo;
+};
 
-usuario.getAll=result =>{
-    mysql.query(
-      "Select * from usuario;", (err, res)=>{
-          if(err){
-              console.log('Error de consulta', err)
-              result(-1, err);
-              return;
-          } else {
-            console.log(res);
-            result(1, res);
-          }
-      }  
+usuario.getAll = result => {
+    sql.query("Select * from usuario;", (err, res) => {
+        if (err) {
+            console.log('Error de consulta', err)
+            result(null, err);
+            return;
+        } else {
+            result(null, res);
+        }
+    }
     );
 };
 
-module.exports= usuario;
+usuario.crearNuevoUsuario = (nuevoUsuario, result) => {
+    sql.query("Insert into usuario SET ?", nuevoUsuario, (err, res) => {
+        if (err) {
+            console.log('Error al crear el usuario', err)
+            result(null, err);
+            return;
+        } else {
+            result(null, res);
+        }
+    }
+    )
+};
+
+usuario.eliminarUsuario= (eliminarUser, result) =>{
+    sql.query("Delete from usuario where ?", eliminarUser, (err, res)=>{
+        if (err) {
+            console.log('Error al eliminar el usuario', err)
+            result(null, err);
+            return;
+        } else {
+            result(null, res);
+        }
+    }) 
+}
+module.exports = usuario;
