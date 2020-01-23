e025d7a3582bd715d3d1e61a43b4c07b67e86e50
CristianPoma <pomacristian421@gmail.com>
2020-01-23 14:30:37 -0500

    23/01/2020

--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
 module.exports =app =>{
-    const usuario= require('./Model/Usuario');
-    app.post('obtenerTodod', usuario.getAll)
+    const usuario= require('./Control/controlador');
+    app.post('/obtenerTodos', usuario.getAll);
+    app.post('/crearUsuario', usuario.crearNuevoUsuario);
+    app.post('/eliminarUser', usuario.eliminarUsuario);
 }
